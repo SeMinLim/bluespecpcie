e54099caf3503ff4782d08ac8ba3b24521ea3801
SeMinLim <seminl1@ics.uci.edu>
2023-07-23 10:05:11 -0700

    changed the code to use dataset

--- examples/haversine/c/haversine.c
+++ examples/haversine/c/haversine.c
@@ -1,3 +1,4 @@
+#include <sys/resource.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,6 +8,33 @@
 #define TO_RADIAN (3.1415926536 / 180)
 
 
+float cities[44691][2];
+float result[44691];
+
+
+// Elapsed time checker
+static inline double timeCheckerCPU(void) {
+        struct rusage ru;
+        getrusage(RUSAGE_SELF, &ru);
+        return (double)ru.ru_utime.tv_sec + (double)ru.ru_utime.tv_usec / 1000000;
+}
+
+// Function for reading benchmark file
+void readBenchmark() {
+	FILE *data;
+	data = fopen("worldcities.csv", "ra");
+
+	int i = 0;
+
+	while (i < 44691) {
+		fscanf(data, "%f,%f\n", &(cities[i][0]), &(cities[i][1]));
+		i++;
+	}
+
+	fclose(data);
+}
+
+// Haversine formula
 float haversine(float lat1, float lon1, float lat2, float lon2)
 {
 	// Distance between latitudes and longitudes
@@ -25,9 +53,23 @@ float haversine(float lat1, float lon1, float lat2, float lon2)
 
 int main()
 {
-	float d = haversine(37.547889, 126.997128, 35.158874, 129.043846);
+	int i = 0;
+
+	readBenchmark();
+	printf( "Read Benchmark File Done!\n" );
+
+	double processStart = timeCheckerCPU();
+	while ( i < 10 ) {
+		result[i] = haversine(cities[8][0], cities[8][1], cities[i][0], cities[i][1]);
+		i++;
+	}
+	double processFinish = timeCheckerCPU();
+	double processTime = processFinish - processStart;
+	printf( "Elapsed Time (CPU): %.2f\n", processTime );
 	
-	printf("dist: %.4f km (%.4f mi.)\n", d, (d/1.609344));
+	// Seoul vs Seoul
+	// The result is gonna be 0km and mi
+	printf("dist: %.4f km (%.4f mi.)\n", result[8], (result[8]/1.609344));
 
 	return 0;
 }
