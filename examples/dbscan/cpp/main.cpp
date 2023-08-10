a89c184e632e0c4422df8a82b026159d403e6500
SeMinLim <seminl1@ics.uci.edu>
2023-08-09 18:29:52 -0700

    modifying host-side sw code

--- examples/dbscan/cpp/main.cpp
+++ examples/dbscan/cpp/main.cpp
@@ -1,157 +1,70 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "bdbmpcie.h"
-//#include "dmasplitter.h"
+#include "dmasplitter.h"
 
+
+#define NumCities 44691
+#define Dimension 2
+
+
+// Elapsed time checker
 double timespec_diff_sec( timespec start, timespec end ) {
 	double t = end.tv_sec - start.tv_sec;
 	t += ((double)(end.tv_nsec - start.tv_nsec)/1000000000L);
 	return t;
 }
 
+// Function for reading benchmark file
+void readfromfile(float* data, char* filename, size_t length) {
+	FILE* f_data = fopen(filename, "rb");
+	if (f_data == NULL ) {
+		printf("File not found: %s\n", filename);
+		exit(1);
+	}
+
+	fread(data, sizeof(float), length, f_data);
+
+	fclose(f_data);
+}
+
+// Main
 int main(int argc, char** argv) {
 	BdbmPcie* pcie = BdbmPcie::getInstance();
-	//DMASplitter* dma = DMASplitter::getInstance();
 
-	//uint32_t size = 128*128;
-
-/*
-	if ( argc > 1 ) {
-		size = atoi(argv[1]);
-	}
-*/
 	unsigned int d = pcie->readWord(0);
 	printf( "Magic: %x\n", d );
-	fflush(stdout);
-	d = pcie->readWord(32);
-	printf( "Dma Addr 0: %x\n", d );
-	fflush(stdout);
+	fflush( stdout );
 
+	char cities_filename[] = "worldcities.bin";
+	float* cities_float = (float*)malloc(sizeof(float)*NumCities*Dimension);
+	uint32_t* cities_uint32 = (uint32_t*)malloc(sizeof(float)*NumCities*Dimension);
 
-	//uint8_t* dmabuf = (uint8_t*)dma->dmaBuffer();
-	uint8_t* dmabuf = (uint8_t*)pcie->dmaBuffer();
-	for ( int i = 0; i < 8*1024; i++ ) {
-		dmabuf[i] = (char)i;
-	}
-	for ( int i = 0; i < 16; i++ ) {
-		dmabuf[i] = 0xaa;
-	}
+	// Read benchmark file	
+	readfromfile(&cities_float[0], cities_filename, NumCities*Dimension);
 
-	timespec start;
-	timespec now;
-	clock_gettime(CLOCK_REALTIME, & start);
-	for ( int i = 0; i < 1024*1024*32; i++ ) {
-		pcie->userWriteWord(0, 8);
+	// Change the type to uint32_t
+	for ( int i = 0; i < NumCities*Dimension; i ++ ) {
+		cities_uint32[i] = *(uint32_t*)&cities_float[i];
 	}
 
-	clock_gettime(CLOCK_REALTIME, & now);
-	double diff = timespec_diff_sec(start, now);
-	printf( "DMA HW->SW elapsed: %f\n", diff );
-	sleep(1);
-
-	printf( "read 0: %x\n", pcie->userReadWord(0) );
-	printf( "read 4: %x\n", pcie->userReadWord(4) );
-	printf( "read 8: %x\n", pcie->userReadWord(8) );
-
-
-	uint32_t* buf32 = (uint32_t*)dmabuf;
-	for ( int i = 0; i < 8; i++ ) {
-		printf( "%d %x\n", i, buf32[i] );
-		buf32[i] = i;
+	// Send data
+	pcie->userWriteWord(0, 0);
+	for ( int i = 0; i < 8; i ++ ) {
+		pcie->userWriteWord(4, cities_uint32[16]);
+		pcie->userWriteWord(4, cities_uint32[17]);
 	}
 
-	clock_gettime(CLOCK_REALTIME, & start);
-	for ( int i = 0; i < 1024*1024*32; i++ ) {
-		pcie->userWriteWord(4, 8);
-	}
+	sleep(10);
 
-	clock_gettime(CLOCK_REALTIME, & now);
-	diff = timespec_diff_sec(start, now);
-	printf( "DMA SW->HW elapsed: %f\n", diff );
-	
-	sleep(1);
-	
-	printf( "read  0: %x\n", pcie->userReadWord(0) );
-	printf( "read  4: %x\n", pcie->userReadWord(4) );
-	printf( "read  8: %x\n", pcie->userReadWord(8) );
-	printf( "read 12: %x\n", pcie->userReadWord(12) );
-	printf( "read 16: %x\n", pcie->userReadWord(16) );
-	printf( "read 20: %x\n", pcie->userReadWord(20) );
-	printf( "read 24: %x\n", pcie->userReadWord(24) );
-	
-	sleep(1);
+	// Get result
+	uint32_t result = pcie->userReadWord(0);
+	uint32_t cycle = pcie->userReadWord(4);
 
-	//printf( "Requesting memread with size %d\n", size );
-	//dma->sendWord(0, size,0,0,0);
+	printf( "Result: %f\n", *(float*)&result );
+	printf( "Total Cycles: %d\n", cycle );
 
-/*
-	while (1) {
-		PCIeWord w = dma->recvWord();
-		printf( "%x %x %x %x %x\n", w.header, w.d[0], w.d[1], w.d[2], w.d[3] );
-		if ( w.header == 0 ) break;
-	}
-*/
-
-
-
-
-/*
-	FlashManager* flash = FlashManager::getInstance();
-
-	uint32_t* pageBufferW = (uint32_t*)malloc(8192+32);
-	uint32_t* pageBufferR = (uint32_t*)malloc(8192+32);
-	for ( int i = 0; i < 8192/4; i++ ) {
-		pageBufferW[i] = i;
-	}
-	printf( "Sending erase message:\n" );
-	flash->eraseBlock(1,1,1);
-	//sleep(1);
-	flash->writePage(1,1,1,0, pageBufferW);
-
-	sleep(5);
-
-	exit(1);
-
-	
-	printf( "\t\tSending read cmd\n" );
-
-
-	flash->readPage(1,1,1,0, pageBufferR);
-	
-	
-	printf( "\t\tStarting read\n" );
-	sleep(1);
-	
-	for ( int i = 0; i < (8192+32)/4; i++ ) {
-		int idx = i % 4;
-		uint32_t r = pcie->readWord((1024+1+idx)*4);
-		printf( "%d:%x ", i, r );
-		if ( i % 8 == 0 ) printf( "\n" );
-	}
-	*/
-
-
-
-/*
-	for ( int i = 0; i < 1024; i++ ) {
-		pcie->writeWord((1024+3)*4, 0);
-	}
-	
-	pcie->writeWord((1024+1)*4, 0);
-
-	pcie->writeWord(1024*4, 4);
-	for ( int i = 0; i < 32; i++ ) {
-		pcie->writeWord(1024*4, 8);
-	}
-	sleep(1);
-	pcie->writeWord((1024+1)*4, 0);
-
-
-	for ( int i = 0; i < 4096; i++ ) {
-		if ( ubuf[i] != 0 ) {
-			printf( "%d: %x\n", i, ubuf[i] );
-		}
-	}
-	*/
+	return 0;
 }
